20dfdc40fa09b12da1d085bbf1d556be58995d47
Ghazal Hafezi <ghazalhafezi@Ghazals-Laptop.local>
2024-01-15 08:48:01 +0330

    requirements and update admin pannle

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from .models import UserProfile , Article , Category
 
-admin.site.register(UserProfile)
-admin.site.register(Article)
-admin.site.register(Category)
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display=['user', 'avatar' , 'description']
+    search_fields=('user',)
+    list_per_page= 2
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display =['title','created_at','author']
+    search_fields=('title',)
+    list_per_page= 2
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display =['title','cover']
+    list_per_page= 3
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, CategoryAdmin)
