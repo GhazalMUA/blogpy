450adf885a741a0d03924a904a29c3eecbe0b993
Ghazal Hafezi <ghazalhafezi@Ghazals-Laptop.local>
2024-01-23 09:48:45 +0330

    template dynamic

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import *
 
-# Create your views here.
+class IndexPage(TemplateView):
+    def get(self, request, **kwargs):
+        article_data =[]
+        articles= Article.objects.all().order_by('-created_at')[:9]
+        for article in articles:
+            article_data.append({
+                
+                    'title':article.title,
+                    'cover':article.cover.url,
+                    'category':article.category.title,
+                    'created':article.created_at.date,
+                    'author' : article.author.user.username
+                
+            })
+        context = {
+            'article_data':article_data
+        }     
+        return render(request, 'index.html', context)
--- blogpy/settings.py
+++ blogpy/settings.py
@@ -121,9 +121,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT= os.path.dirname(os.path.abspath(__file__))
+STATICFILES_DIRS=(
+    os.path.join(BASE_DIR,'static'),
+)
 
 
 CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
@@ -134,3 +136,7 @@ CKEDITOR_UPLOAD_PATH = "uploads/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#media
+MEDIA_URL = 'files/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
